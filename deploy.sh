6668928a59bb93f4b512f74ee7a4e1f359935150
Martin Noya <martinnoya@gmail.com>
2018-09-17 00:03:51 -0300

    Updated deploy script for uwsgi reload

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
 git -C ~/deckbuilder reset --hard HEAD
 git -C ~/deckbuilder/ pull
 source ~/venvs/deckbuilder/bin/activate
 python ~/deckbuilder/manage.py migrate
 python ~/deckbuilder/manage.py collectstatic --noinput
 sudo /etc/init.d/nginx restart
+uwsgi --reload /tmp/project-master.pid
